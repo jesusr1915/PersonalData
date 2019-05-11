a6776a919e978300a1fe2075830485bb99b8d58c
Jesus Romero <jesus.rg1915@gmail.com>
2019-05-11 15:51:38 -0500

    Personal data ready!

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const app = express();
+const APP = express();
+const PORT = process.env.PORT || 3000
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (request, response) => {
+
+APP.get('/personal-data', (request, response) => {
 
     const res = {
-
         name: 'Jesus',
+        lastName: 'Romero',
         age: 28,
-        status: null,
-        status2: undefined,
-        url: request.url
+        single: true,
+        mobileNumber: '5591927209',
+        collegeCareer: 'Engineer in Communications and Electronics (INSTITUTO POLITECNICO NACIONAL)',
+        work: 'Spotlight Santander (México) Senior Frontend',
+        knowledges: 'Java, JS, TS, C#, React, React Native, Angular, NodeJS, Express, Unity, Git, Subversion, Microcontrollers, Electronic',
+        git: 'https://github.com/jesusr1915',
+        gitlab: 'https://gitlab.com/jesusrg',
+        youtube: 'https://www.youtube.com/channel/UCsoFIUw7QudMIWgfIIj9DjA'
     }
-
 
     response.send(res);
 });
 
-app.listen(3000, () => console.log('Server run in port 3000'))
+APP.listen(PORT, () => console.log(`Server run in port ${PORT}`))
